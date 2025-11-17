0fbf17db148b04fdf757696af40d9b5078a49388
VageLO <48802151+VageLO@users.noreply.github.com>
2025-11-18 01:18:19 +0300

    update: v1.0.0

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/VageLO/bsmmex
 go 1.24.10
 
 require (
-	github.com/VageLO/bsparse v1.0.0
+	github.com/VageLO/bsparse v1.0.1
 	github.com/fsnotify/fsnotify v1.9.0
 )
 
 require (
 	github.com/dslipak/pdf v0.0.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"log/syslog"
+	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/VageLO/bsparse/parse"
 	"github.com/fsnotify/fsnotify"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var syslogLogger *log.Logger
+
+var mmexFile string
+
+type CATEGORY_V1 struct {
+	CATEGID int64
+	CATEGNAME string
+}
 
 // Watch for CREATE event in specified folder
 func WatchFolder(folder string) {
@@ -45,20 +57,22 @@ func watchLoop(w *fsnotify.Watcher) {
 				return
 			}
 
-			syslogLogger.Println("EVENT:", event)
+			//log.Println("EVENT:", event)
 
 			if event.Has(fsnotify.Create) {
 				ext := filepath.Ext(event.Name)
 				if !strings.EqualFold(ext, ".pdf") {
-					syslogLogger.Println("WARNING:", "file extension is not .pdf")
+					//log.Println("WARNING:", "file extension is not .pdf")
 					continue
 				}
 
 				syslogLogger.Printf("Processing file %s", event.Name)
 				transactions := bsparse.GetTransactions(event.Name)
 
-				syslogLogger.Println("Saving csv file")
-				bsparse.Csv(transactions)
+				uploadToMMEX(transactions)
+
+				//syslogLogger.Println("Saving csv file")
+				//bsparse.Csv(transactions)
 			}
 		case err, ok := <-w.Errors:
 			if !ok {
@@ -67,6 +81,106 @@ func watchLoop(w *fsnotify.Watcher) {
 			}
 		}
 	}
+}
+
+func uploadToMMEX(transactions bsparse.TransactionSlice) {
+
+	db, err := sql.Open("sqlite3", mmexFile)
+	if err != nil {
+		syslogLogger.Panicln(err)
+	}
+	defer db.Close()
+
+	// name for category and payee
+	name := "Unknown"
+
+	selectAccountID := "SELECT ACCOUNTID FROM ACCOUNTLIST_V1 WHERE ACCOUNTNAME = ?"
+
+	selectCategoryID := "SELECT CATEGID FROM CATEGORY_V1 WHERE CATEGNAME = ?"
+	insertCategory := "INSERT INTO CATEGORY_V1 (CATEGNAME) VALUES (?)"
+
+	selectPayeeID := "SELECT PAYEEID FROM PAYEE_V1 WHERE PAYEENAME = ?"
+	insertPayee := "INSERT INTO PAYEE_V1 (PAYEENAME) VALUES (?)"
+
+	accountId, err := getId(db, selectAccountID, "", "AlfaBank")
+	if err != nil {
+        syslogLogger.Println(err)
+        return
+    }
+
+	categoryId, err := getId(db, selectCategoryID, insertCategory, name)
+	if err != nil {
+        syslogLogger.Println(err)
+        return
+    }
+
+	payeeId, err := getId(db, selectPayeeID, insertPayee, name)
+	if err != nil {
+        syslogLogger.Println(err)
+        return
+    }
+
+	insertTransaction := `
+		INSERT INTO CHECKINGACCOUNT_V1 (
+		ACCOUNTID, TOACCOUNTID, PAYEEID, TRANSCODE, TRANSAMOUNT,
+		STATUS, TRANSACTIONNUMBER, NOTES, CATEGID, TRANSDATE
+		) VALUES (?, -1, ?, ?, ?, '', '', ?, ?, ?)
+	`
+	for index, transaction := range transactions {
+		syslogLogger.Printf("Inserting %d transaction: %s", index+1, transaction.Id)
+
+		typeOf := "Withdrawal"
+
+		result, err := db.Exec(
+			insertTransaction,
+			accountId,
+			payeeId,
+			typeOf,
+			transaction.Price,
+			transaction.Description,
+			categoryId,
+			transaction.Date,
+		)
+		if err != nil {
+			syslogLogger.Println(err)
+			return
+		}
+
+		insertedId, err := result.LastInsertId()
+		if err != nil {
+			syslogLogger.Println(err)
+			return
+		}
+		syslogLogger.Printf("Inserted ID - %d in database", insertedId)
+	}
+}
+
+func getId(db *sql.DB, selectQuery string, insertQuery string, name string) (int64, error) {
+	row := db.QueryRow(selectQuery, name);
+
+	var id int64
+
+	err := row.Scan(&id)
+	if insertQuery == "" && errors.Is(err, sql.ErrNoRows) {
+		return 0, fmt.Errorf("Account %s not found in MMEX database.", name)
+	} else if errors.Is(err, sql.ErrNoRows) {
+		return insert(db, insertQuery, name)
+	} else if err != nil {
+        return 0, err
+    }
+    return id, nil
+}
+
+// Insert category and payee into database
+func insert(db *sql.DB, query string, name string) (int64, error) {
+
+	result, err := db.Exec(query, name)
+    if err != nil {
+		syslogLogger.Println(err)
+        return 0, err
+    }
+
+	return result.LastInsertId()
 }
 
 func main() {
@@ -78,6 +192,19 @@ func main() {
 
 	// Create a logger that outputs to syslog
 	syslogLogger = log.New(syslogWriter, "SYSLOG: ", log.Ldate|log.Ltime)
-	// TODO: refine
-	WatchFolder("/home/vagelo/Workflow/statements")
+
+	bsfolderEnv := "BSFOLDER"
+	mmexFileEnv := "MMEXFILE"
+
+	bsfolder, exist := os.LookupEnv(bsfolderEnv)
+	if !exist {
+		syslogLogger.Fatalf("ENV variable %s is empty", bsfolderEnv)
+	}
+
+	mmexFile, exist = os.LookupEnv(mmexFileEnv)
+	if !exist {
+		syslogLogger.Fatalf("ENV variable %s is empty", mmexFileEnv)
+	}
+
+	WatchFolder(bsfolder)
 }
